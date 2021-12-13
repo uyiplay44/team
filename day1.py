a509f02f06184c1777f26089437d3586a5b22a90
uyiplay44 <91386330+uyiplay44@users.noreply.github.com>
2021-12-13 13:27:36 +0100

    message
    python

---
+++ day1.py
@@ -0,0 +1,65 @@
+# # DATATYPES
+# int: 1, 2 3, 4, -7
+# str: "Rhame", 'hello', '2'
+# bool: True, False
+# float: 0.35, 1.44
+
+# VARIABLES
+# name = 'David'
+# print(name)
+
+# name = 'Wisdom'
+# print(name)
+
+# age = 28
+# print(age)
+
+# lecture = "pythonfullstack"
+# print(lecture)
+
+# networth =  '77.7'
+# print(networth)
+
+# print ('Hello , what is your name?')
+# name = input()
+# print ('Hello', name)
+
+# print ('How many eggs are there in the basket?')
+# name = input()
+# print ('value are', name)
+
+
+#----BASIC OPERATORS----
+
+# + Addition (input model that make it work)
+# - Substraction
+# / Divison
+# * Multiplication
+
+#-----STREAMS--------
+# num1 = '3'
+# num2 = '8'
+
+# print(num1)
+# print(num2)
+
+# print(num2 + num1)
+
+# SUM = int (num2) + int (num1)
+# print (SUM)
+# # OR 
+# print (int(num2) + int(num1))
+
+
+num1 = 3
+# num2 = 8
+num2 = 9
+
+print(num1)
+print(num2)
+print(num2 + num1)
+print(num2 - num1)
+print(num2 * num1)
+print(num1 // num2)
+print(num2 / num1)
+print(num2 // num1)
